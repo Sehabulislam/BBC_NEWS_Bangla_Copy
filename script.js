f733004885267f94bf4c731d33f6a590af48748d
Sehabulislam <shehabulislam244@gmail.com>
2025-09-14 21:15:29 +0600

    added the categories section

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const displayCategories = (categories)=>{
     categories.forEach(catg => {
         const li = document.createElement("div");
         li.innerHTML = `
-        <li id='${catg.id}' class="hover:border-b-4 hover:border-red-500 border-red-700 cursor-pointer">${catg.title}</li>
+        <li id='${catg.id}' class="hover:border-b-4 hover:border-red-500 border-red-600 cursor-pointer">${catg.title}</li>
         `
         categoriesBtn.appendChild(li);
     })
@@ -30,9 +30,43 @@ const displayCategories = (categories)=>{
         })
 
         if(e.target.localName === "li"){
-            console.log(e.target);
-            e.target.classList.add("border-b-4")
+            // console.log(e.target.id);
+            e.target.classList.add("border-b-4");
+            loadCategoriesNews(e.target.id)
         }
     })
 }
+
+const loadCategoriesNews =async(newsid)=>{
+    try{
+        const url =`https://news-api-fs.vercel.app/api/categories/${newsid}`
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCategoriesNews(data.articles);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+const displayCategoriesNews =(news)=>{
+    const newsContainer = document.getElementById("newsContainer");
+    newsContainer.innerHTML = "";
+    news.forEach(newsdata =>{
+        // console.log(newsdata);
+        const div = document.createElement("div");
+        div.innerHTML = `
+                <div class="w-full p-2 flex flex-row md:flex-col">
+                    <img  src="${newsdata.image.srcset[5].url}" alt="${newsdata.image.alt}" class=" w-[160px] md:w-full h-30 mr-3 md:mr-0 object-cover ">
+                <div class="mt-3 space-y-2">
+                    <h2 class="font-semibold">${newsdata.title}</h2>
+                <p class="text-sm text-gray-600">${newsdata.time}</p>
+                </div>
+                </div>
+        `
+        newsContainer.appendChild(div)
+    })
+}
+loadCategoriesNews("main");
+
 loadCategories()
